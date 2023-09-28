f51c1df743d1be85509925a712606411f889c7ca
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL>
2023-09-28 15:11:25 -0400

    compiles

--- create/src/create.rs
+++ create/src/create.rs
@@ -1,8 +1,8 @@
-use serde_json::Value;
+use serde::{Deserialize, Serialize};
 use std::error::Error;
-
 use reqwest::blocking::Client;
 
+#[derive(Debug, Serialize, Deserialize)]
 struct DatabaseObject {
     name: String,
     id: i32,
@@ -10,11 +10,7 @@ struct DatabaseObject {
     language: String,
 }
 
-struct PostBody {
-    json: DatabaseObject,
-}
-
-pub fn insert_hardcoded() -> Result<Value, Box<dyn Error>> {
+pub fn insert_hardcoded() -> Result<String, Box<dyn Error>> {
     // Base parts of connection string
     let rest_base = "http://localhost:8080/v1";
     let kvs_endpoint = format!("{}/kvs", rest_base);
@@ -26,21 +22,18 @@ pub fn insert_hardcoded() -> Result<Value, Box<dyn Error>> {
 
     let record_uri = format!("{}/{}/{}/{}", kvs_endpoint, namespace, setname, userkey);
 
-    // The content to be stored into Aerospike.
-    let obj = DatabaseObject {
-        name: "Bob".to_string(),
-        id: 123,
-        color: "Blue".to_string(),
-        language: "Ingrish".to_string(),
-    };
+    let request_body = r#"{
+        "name": "James",
+        "id": 123,
+        "color": "Blue,
+        "language": "Ingrish"
+    }"#;
 
-    let post_body = PostBody { json: obj };
+    let client = Client::new();
 
-    let client = reqwest::Client::new();
+    let response = client.post(record_uri).body(request_body).send()?;
 
-    let response = client.post(record_uri).body(post_body).send();
+    println!("{:?}", response);
 
-    let serde_result = serde_json::from_str::<Value>(&response)?;
-
-    Ok(serde_result)
+    Ok("it werked.".to_string())
 }
--- create/src/main.rs
+++ create/src/main.rs
@@ -3,14 +3,9 @@ use std::error::Error;
 mod create;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    
-    let response = create::get_some_json()?;
+    let response = create::insert_hardcoded()?;
 
-    println!("dynamic_json: {:#?}", response);
-
-    let struct_deserialised_json = create::get_some_json()?;
-
-    println!("struct_deserialised_json: {:#?}", struct_deserialised_json);
+    println!("result: {:#?}", response);
 
     Ok(())
 }
